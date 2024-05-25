606b0d286b06055e927a6414edb176e81728c9ec
Onkar Karale <onkarkarale4@gmail.com>
2024-05-25 22:47:06 +0530

    Get the first and last item in an array using JavaScript

---
+++ Array Programs/FirstLast.js
@@ -0,0 +1,17 @@
+// Get the first and last item in an array using JavaScript :
+
+// Array 
+let s = [3, 2, 3, 4, 5]
+function Arr () {
+
+  // Storing the first item in a variable 
+  let first = s[0]
+
+  // Storing the last item 
+  let last = s[s.length - 1]
+
+  // Printing output to screen
+  console.log('First element is ' + first)
+  console.log('Last element is ' + last)
+}
+Arr() // Calling the function
