1061b2dee42802981352a68a52d8ef44bce332ef
Onkar Karale <onkarkarale4@gmail.com>
2024-05-26 23:36:33 +0530

    JavaScript Program to Sort Words in Alphabetical Order

---
+++ Array Programs/SortWords.js
@@ -0,0 +1,22 @@
+// JavaScript Program to Sort Words in Alphabetical Order
+
+// Using the Array sort() method :
+// The sort() method sorts all the elements of the array and returns the sorted array.
+// By default, the sort() method sorts the elements in alphabetical order.
+
+const words = [
+  'JavaScript',
+  'Program',
+  'to',
+  'Sort',
+  'Words',
+  'in',
+  'Alphabetical',
+  'Order'
+]
+
+// Sorting the input array using array.sort 
+const sortedWords = words.sort()
+
+// Getting the sorter array output 
+console.log(sortedWords)
