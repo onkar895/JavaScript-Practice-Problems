b85372d401bacfafd56b42c0d373bf26eeeb78c3
Onkar Karale <onkarkarale4@gmail.com>
2024-05-27 00:27:55 +0530

    Replace an item from an array in JavaScript

---
+++ Array Programs/ReplaceElement.js
@@ -0,0 +1,20 @@
+// Replace an item from an array in JavaScript :
+
+// Using Array Indexing :
+let arr = [20, 30, 40, 50]
+
+const index = 3
+const newVal = 70
+
+arr[index] = newVal
+console.log(arr)
+
+// Using Splice method :
+
+const list = ['jan', 'feb', 'April', 'May']
+
+list.splice(2, 2, 'march')
+
+list.splice(3, 3, 'April')
+
+console.log(list)
