aeabd25099d48fb75f8cd0620d6c85603f24ce40
Onkar Karale <onkarkarale4@gmail.com>
2024-05-26 23:50:40 +0530

    Remove Duplicate Elements from JavaScript Array

--- Array Programs/RemoveDuplicate.js
+++ Array Programs/RemoveDuplicate.js
@@ -22,3 +22,18 @@ function removeDuplicates (arr1) {
 }
 
 console.log(removeDuplicates(arr1))
+
+// Using forEach method :
+
+let Arr = [300, 500, 200, 600, 300, 500, 1000, 250, 600]
+
+function removeDuplicate (Arr) {
+  let unique = []
+  Arr.forEach(element => {
+    if (!unique.includes(element)) {
+      unique.push(element)
+    }
+  })
+  return unique
+}
+console.log(removeDuplicate(Arr))
